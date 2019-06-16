1a2b22e57d148fddbaade06d938e6bec57f22fa9
dreamcheck <main.dreamcheck@gmail.com>
2019-06-16 19:49:00 +0800

    lecture11 - learn grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,7 +4,31 @@
 
     <!-- team content -->
     <v-container class="my-5">
-      content
+
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+      </v-layout>
+
     </v-container>
 
   </div>
