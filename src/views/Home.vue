09a08e812d1c17c458c51a33397ab4574ca1b85a
dreamcheck <main.dreamcheck@gmail.com>
2019-06-16 17:37:15 +0800

    lecture4 - breakpoins and visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,12 @@
   <div class="home">
     <h1>Home</h1>
 
-    <v-btn flat class="red white--text">click me</v-btn>
-    <v-btn depressed dark color="red">click me</v-btn>
-    <v-btn flat color="red">click me</v-btn>
+    <v-btn depressed class="hidden-md-and-down" color="pink white--text">click me</v-btn>
 
-    <v-btn depressed color="red white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <v-btn depressed class="hidden-md-and-up" color="green white--text">click me</v-btn>
 
-    <v-btn small depressed color="red white--text">
-      <v-icon small left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <v-btn depressed class="hidden-sm-only" color="purple white--text">click me</v-btn>
 
-    <v-btn large depressed color="red white--text">
-      <span>email me</span>
-      <v-icon large right>email</v-icon>
-    </v-btn>
-
-    <v-btn depressed fab small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
   </div>
 </template>
 
