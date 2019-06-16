da31321b0e3c28f6dd4eaef51041d8654a172829
dreamcheck <main.dreamcheck@gmail.com>
2019-06-16 16:37:51 +0800

    lecture2 - text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+
+    <!-- Red background and white text -->
+    <p class="red white--text">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, odit!
+    </p>
+
+    <!-- Make pink background lighten and red text darken -->
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, maiores!
+    </p>
+
+    <!-- The biggest display text -->
+    <h1 class="display-4">Massive Text</h1>
+
+    <!-- The smallest displat text -->
+    <h4 class="display-1">Small Text</h4>
+
+    <!-- headline text -->
+    <p class="headline">this is headline</p>
+
+    <!-- subheading text -->
+    <p class="subheading">this is subheading</p>
+
+    <!-- caption -->
+    <p class="caption">this is caption</p>
   </div>
 </template>
 
