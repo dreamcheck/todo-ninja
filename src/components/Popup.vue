0587e6455c2ebfd27b261c041f00a6ecc202a878
dreamcheck <main.dreamcheck@gmail.com>
2019-06-17 05:43:29 +0800

    lecture22 - basic form on dialog

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- dialog -->
-    <v-dialog width="500">
+    <v-dialog width="500" flat>
 
       <!-- activator slot -->
       <template v-slot:activator="{ on }">
@@ -12,9 +12,19 @@
 
       <!-- popup content -->
       <v-card>
+        <!-- title -->
         <v-card-title primary-title>
           Add a New Project
         </v-card-title>
+
+        <!-- content -->
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field label="Title" v-model="title"></v-text-field>
+            <v-textarea label="Information" v-model="content"></v-textarea>
+            <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
@@ -23,5 +33,16 @@
 <script>
 export default {
   name: 'popup',
+  data() {
+    return {
+      title: '',
+      content: '',
+    };
+  },
+  methods: {
+    submit() {
+      console.log({ title: this.title, content: this.content });
+    },
+  },
 };
 </script>
